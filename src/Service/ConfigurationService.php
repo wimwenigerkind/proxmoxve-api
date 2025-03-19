b691a08095dd026237cd8a985ad607166587e871
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-03-19 19:28:47 +0100

    WIP

--- src/Service/ConfigurationService.php
+++ src/Service/ConfigurationService.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the WimDevGroup/proxmoxve-api package.
+ *
+ * (c) Wim Wenigerkind <wim.wenigerkind@heptacom.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Wimdevgroup\ProxmoxveApi\Service;
 
 class ConfigurationService
